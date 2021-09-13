8a6446cb5b84f5ac95f0675a7c5f2ecc75e25183
Spy <89559710+Spy06@users.noreply.github.com>
2021-09-13 17:25:41 +0700

    Add files via upload

---
+++ Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Conn_Client
+{
+	class MainClass
+	{
+		private static TcpClient client;
+		private static string IP = "127.0.0.1";
+		private static int port = 80;
+		public static void Main (string[] args)
+		{
+			IPEndPoint endPoint = new IPEndPoint (IPAddress.Parse (IP), port);
+			client = new TcpClient {
+				SendBufferSize = 1024,
+				ReceiveBufferSize = 1024
+			};
+			client.Connect (endPoint);
+
+			Console.WriteLine ("Connecting");
+
+			string msg = Console.ReadLine ();
+
+			byte[] data = Encoding.ASCII.GetBytes (msg);
+			NetworkStream stream = client.GetStream ();
+			stream.Write (data, 0, data.Length);
+			stream.Flush ();
+		}
+	}
+}
---
+++ Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Conn_Server
+{
+	class MainClass
+	{
+		private static TcpListener tcpListener;
+
+		public static void Main (string[] args)
+		{
+			tcpListener = new TcpListener (IPAddress.Any, 80);
+			tcpListener.Start ();
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine ("Server Started, Listening...");
+
+			TcpClient client = tcpListener.AcceptTcpClient ();
+			Console.WriteLine ("Incoming connection");
+
+			NetworkStream stream = client.GetStream ();
+			byte[] buffer = new byte[1024];
+			int data = stream.Read (buffer, 0, buffer.Length);
+			byte[] message = new byte[data];
+			Array.Copy (buffer, message, data);
+			string msg = Encoding.ASCII.GetString (message);
+			Console.WriteLine (msg);
+		}
+	}
+}
